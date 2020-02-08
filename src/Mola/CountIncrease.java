fb75baeb16dfeb120ef1134278436067e07387a0
Rajitha Bhanuka <halovitage.bhanuka@pearson.com>
2020-02-08 18:54:17 +0530

    codes added

---
+++ src/Mola/CountIncrease.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mola;
+
+/**
+ *
+ * @author bhanuka
+ */
+public class CountIncrease {
+    
+    static int count =0;
+
+    public CountIncrease() {
+        count++;
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++) {
+            CountIncrease t = new CountIncrease();
+        }
+        
+        System.out.println(count);
+    }
+    
+}
